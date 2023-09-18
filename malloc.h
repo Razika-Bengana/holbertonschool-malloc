4186aadefc784b8b5df97a5b0fd27556cfb11e76
Razika-Bengana <5003@holbertonstudents.com>
2023-09-18 15:10:03 +0200

    Header file

---
+++ malloc.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+size_t get_page_size(void);
+size_t align_size(size_t size);
+void *naive_malloc(size_t size);
+
+#endif /* MAIN_H */
