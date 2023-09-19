455857dcf1b316799dbc81971fbe9e1c0cd9b74d
Razika-Bengana <5003@holbertonstudents.com>
2023-09-19 12:36:10 +0200

    Update the Header file for free function

--- malloc.h
+++ malloc.h
@@ -5,9 +5,16 @@
 #include <unistd.h>
 #include <stdint.h>
 
+/* task 0 */
 size_t get_page_size(void);
 size_t align_size(size_t size);
 void *naive_malloc(size_t size);
+
+/* task 1 */
 void *_malloc(size_t size);
+
+/* task 2 */
+void _free(void *ptr);
+
 
 #endif /* MAIN_H */
